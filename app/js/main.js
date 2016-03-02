c3bf805db82cf3ff2677b4180c1948087f8de05b
diwanee <diwanee@Mac-Lutalica.local>
2016-03-02 15:34:37 +0100

    shooting and aim

--- app/js/main.js
+++ app/js/main.js
@@ -3,13 +3,14 @@
 \*========================*/
 
 var canvas = document.getElementById("context");
-canvas.width = 1000;
-canvas.height = 275;
-canvas.style.width = canvas.width + "px";
-canvas.style.height = canvas.height + "px";
 var context = canvas.getContext("2d"),
 	width = 1000,
-	height = 275;
+	height = 700;
+canvas.width = width;
+canvas.height = height;
+canvas.style.width = canvas.width + "px";
+canvas.style.height = canvas.height + "px";
+
 
   // Add keys to array
 
@@ -23,13 +24,15 @@ var context = canvas.getContext("2d"),
   	delete keys[e.keyCode];
   }, false);
 
-    window.addEventListener("mousemove", function(e){
-      deltaX = event.clientX -10 - player.x;
-      deltaY = event.clientY -10- player.y;
-      console.log(deltaY);
-      player.hand.rotation =   180/Math.PI * Math.atan2(deltaY, deltaX);
-      // console.log( player.hand.rotation);
-    }, false);
+  window.addEventListener("mousemove", function(e){
+		mouseX = event.clientX;
+		mouseY = event.clientY;
+		findMouseAngle();
+  }, false);
+	window.addEventListener("click", function(e){
+		shoot();
+		console.log(bullets);
+	}, false);
 
 /*========================*\
   #Variables
@@ -38,6 +41,7 @@ var loop,
     counter = 0,
     gameIsOver =false,
     enemies = [],
+		bullets = [],
     gameSpeed = 1,
     keys = [],
     bgpos = 0,
@@ -47,8 +51,12 @@ var loop,
     // Init images
   	images = [],
   	requiredImages = 0,
+		mouseX,
+		mouseY,
   	doneImages = 0;
-
+	var vB,
+	 vBx,
+	 vBy;
 var player = {
   width:20,
   height:20,
@@ -58,20 +66,21 @@ var player = {
     rotation:0
   }
 }
-initImages(["road1.png"]);
+// initImages(["road1.png"]);
 var enemy = function(){
 	this.x = width  + Math.random()*100;
-	this.y= 25 + Math.floor(Math.random()*4)*50;
+	this.y= 25 + Math.floor(Math.random()*12)*50;
 	this.width= 24;
 	this.height= 50;
-	this.isMoving = false;
-	this.movingTime= 20;
-	this.movingSpeed= 4;
-	this.health= 5;
-	this.isDead = false;
-	this.maxHealth = 5;
-	this.name_id= "normal";
-	this.score = 4;
+}
+var bullet = function(x,y){
+	this.x = x;
+	this.y = y;
+	this.width= 3;
+	this.height= 3;
+	this.angle = player.hand.rotation * Math.PI / 180;
+	this.deltaX = deltaX;
+
 }
 var start1;
 function start(){
@@ -104,22 +113,28 @@ if (counter % 50 == 0 ){
   enemies.push(new enemy());
 }
 
+for (i in bullets){
+	vB = 22 //Speed of bullet
+	vBx = vB*Math.cos(bullets[i].angle); //Speed of bullet on x-axis
+	vBy = vB*Math.sin(bullets[i].angle); //Speed of bullet on y-axis
+	bullets[i].x+=vBx;
+	bullets[i].y+=vBy;
+}
 
-
-	if(keys[37] && !gameIsOver){player.x-=5;} // Left
-	if(keys[39] && !gameIsOver){player.x+=5;} // Right
-	if(keys[38] && !gameIsOver ){player.y-=5; } // Up
-	if(keys[40] && !gameIsOver ){player.y+=5; } // Down
-	// if(keys[13] && gameIsOver){ chooseLevel(); } // Start new game
+if(keys[37] && !gameIsOver){player.x-=5;findMouseAngle();} // Left
+if(keys[39] && !gameIsOver){player.x+=5;findMouseAngle();} // Right
+if(keys[38] && !gameIsOver ){player.y-=5;findMouseAngle(); } // Up
+if(keys[40] && !gameIsOver ){player.y+=5;findMouseAngle(); } // Down
+// if(keys[13] && gameIsOver){ chooseLevel(); } // Start new game
 
 /* ---------------*\
-   #Boudaries
+ #Boudaries
 \* ---------------*/
 
-	if(player.x < 0){player.x=0} // Left margin
-	if(player.y <= 25){player.y=25} // Top margin
-	if(player.x >= width - player.width){player.x=width - player.width}  // Right margin
-	if(player.y >= height - player.height - 50){player.y=height-player.height -50; } // Left margin
+if(player.x < 0){player.x=0} // Left margin
+if(player.y <= 25){player.y=25} // Top margin
+if(player.x >= width - player.width){player.x=width - player.width}  // Right margin
+if(player.y >= height - player.height - 50){player.y=height-player.height -50; } // Left margin
 
 }
 function render(){
@@ -140,11 +155,15 @@ function render(){
   context.fillRect(0, 0 , 50, 4);
   context.fillStyle='black';
   context.restore();
-  context.fillRect(0, 225, width, 50);
+  context.fillRect(0, height - 50, width, 50);
   for (i in enemies){
     var enemy = enemies[i];
     context.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
   }
+	for (i in bullets){
+		var bullet = bullets[i];
+		context.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+	}
 }
 function initImages(paths){
 	requiredImages = paths.length;
@@ -159,4 +178,20 @@ function initImages(paths){
 		}
 	}
 }
+function findMouseAngle(){
+	deltaX = mouseX -10 - player.x;
+	deltaY = mouseY -10- player.y;
+	player.hand.rotation =   180/Math.PI * Math.atan2(deltaY, deltaX);
+	console.log("searching");
+}
+function shoot(){
+	bullets.push(new bullet(player.x,player.y));
+}
+function collision(first, second){
+	return !(first.x > second.x + second.width ||
+		first.x+first.width<second.x ||
+		first.y > second.y + second.height ||
+		first.y+first.height<second.y);
+}
+findMouseAngle();
 start();
