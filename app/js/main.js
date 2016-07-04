6a66ebe9ed7fe7ec8ce06443fd377da2fa37d41e
Vasilije Milenkovic <vasilie@Vasilijes-MacBook-Pro.local>
2016-07-04 21:17:19 +0200

    mew

--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,8 @@ canvas.width = width;
 canvas.height = height;
 canvas.style.width = canvas.width + "px";
 canvas.style.height = canvas.height + "px";
-
+var score = 0;
+var gameIsOver = false;
 
   // Add keys to array
 
@@ -62,19 +63,29 @@ var player = {
   height:20,
   y:100,
   x:50,
+	health:100,
+	color:'rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+")",
   hand:{
     rotation:0
   }
 }
+$("#context").css({"border-color":player.color});
 // initImages(["road1.png"]);
 var enemy = function(){
 	this.x = width  + Math.random()*100;
 	this.y= 25 + Math.floor(Math.random()*12)*50;
 	this.width= 24;
 	this.height= 24;
-	this.color='#FF9800';
+	this.color='rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')';
 	this.speed = 3;
 }
+var sound_shoot = [];
+var sound_shoot_counter = 0;
+for (i=0;i<12;i++){
+	 sound_shoot.push(new Audio("sound/shoot.wav"));
+	 sound_shoot[i].volume = 0.05;
+	 // sound_tank_bullet[i].volume = sound_tank_bullet_volume;
+ }
 var bullet = function(x,y){
 	this.x = x;
 	this.y = y;
@@ -87,6 +98,7 @@ var bullet = function(x,y){
 	this.directionY = 1;
 	this.vBx = 1;
 	this.vby = 1;
+	this.decay = 4;
 
 }
 var start1;
@@ -114,6 +126,8 @@ for (i in enemies){
 	for (j in bullets){
 		if (collision(enemies[i],bullets[j])){
 			enemies.splice(i,1);
+			score++;
+			$(".score").html(score);
 		}
 	}
 }
@@ -137,7 +151,16 @@ for (i in enemies){
 if (counter % 50 == 0 ){
   enemies.push(new enemy());
 }
-
+for (i in enemies){
+	if (collision(enemies[i],player)){
+		enemies.splice(i,1);
+		player.health -= 25;
+		if (player.health <= 0){
+			player.health = 0;
+			gameOver();
+		}
+	}
+}
 for (i in bullets){
 	vB = 22 //Speed of bullet
 	vBx = vB*Math.cos(bullets[i].angle); //Speed of bullet on x-axis
@@ -147,9 +170,17 @@ for (i in bullets){
 	bullets[i].y+=vBy*bullets[i].directionY;
 	if (bullets[i].x >= width || bullets[i].x <= 0){
 		bullets[i].directionX *= -1;
+		bullets[i].decay-=1;
+		if (bullets[i].decay<=0){
+			bullets.splice(i,1);
+		}
 	}
 	if (bullets[i].y <= 25 || bullets[i].y>=height-53){
 		bullets[i].directionY *= -1;
+		bullets[i].decay-=1;
+		if (bullets[i].decay<=0){
+			bullets.splice(i,1);
+		}
 	}
 }
 
@@ -158,6 +189,7 @@ if(keys[37] || keys[65] && !gameIsOver){player.x-=5;findMouseAngle();} // Left
 if(keys[39] || keys[68] && !gameIsOver){player.x+=5;findMouseAngle();} // Right
 if(keys[38] || keys[87] && !gameIsOver ){player.y-=5;findMouseAngle(); } // Up
 if(keys[40] || keys[83] && !gameIsOver ){player.y+=5;findMouseAngle(); } // Down
+if(keys[13] && gameIsOver){newGame();}
 // if(keys[13] && gameIsOver){ chooseLevel(); } // Start new game
 
 /* ---------------*\
@@ -174,7 +206,17 @@ function render(){
   context.clearRect(0, 0, width, height);
 	context.fillStyle='black';
   context.fillRect(0, 0, width, 25);
-	context.fillStyle='purple';
+	context.fillText("Score",10,50);
+	context.fillStyle="red";
+	context.fillRect(5,10,200, 5);
+	context.fillStyle=player.color;
+
+	context.fillRect(5,10,player.health*2, 5);
+
+	context.fillStyle='white';
+	context.fillText(player.health+'%',210,16);
+
+	context.fillStyle=player.color;
   context.fillRect(player.x, player.y, player.width, player.height);
   context.save();
   context.translate(player.x, player.y);
@@ -192,6 +234,7 @@ function render(){
   context.restore();
   context.fillStyle='black';
   context.fillRect(0, height - 50, width, 50);
+
   for (i in enemies){
     var enemy = enemies[i];
 		context.fillStyle=enemy.color;
@@ -202,7 +245,21 @@ function render(){
 		var bullet = bullets[i];
 		context.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
 	}
+
 	context.fillStyle='purple';
+	if (gameIsOver){
+		context.fillStyle='white';
+		context.font='50px Press';
+		context.fillText("GAME OVER",width/2-220,height/2+25);
+		context.font='15px Press';
+		context.fillText("YOUR SCORE: "+score,width/2-220,height/2+50);
+		context.font='12px Press';
+		context.fillText("PRESS ENTER FOR NEW GAME",width/2-220,height/2+100);
+		context.font='10px sans-serif';
+	}
+	context.fillStyle = player.color;
+	context.fillRect(mouseX-10, mouseY, 21, 1);
+	context.fillRect(mouseX, mouseY-10, 1, 21);
 }
 function initImages(paths){
 	requiredImages = paths.length;
@@ -230,7 +287,12 @@ function findEnemyMovingAngle(enemy,player){
 	return moving_angle;
 }
 function shoot(){
-	bullets.push(new bullet(player.x,player.y));
+	if (!gameIsOver && bullets.length < 10){
+		bullets.push(new bullet(player.x,player.y));
+		sound_shoot[sound_shoot_counter].play();
+		sound_shoot_counter++;
+		sound_shoot_counter = sound_shoot_counter %10;
+	}
 }
 function collision(first, second){
 	return !(first.x > second.x + second.width ||
@@ -245,6 +307,17 @@ function hCollision(first, second){
 function vCollision(first, second){
 	return !(first.y > second.y + second.height ||
 	first.y+first.height<second.y);
+}
+function gameOver(){
+	gameIsOver = true;
+}
+function newGame(){
+	bullets = [];
+	enemies = [];
+	player.health = 100;
+	gameIsOver = false;
+	player.x = 50;
+	player.y = 100;
 }
 findMouseAngle();
 start();
